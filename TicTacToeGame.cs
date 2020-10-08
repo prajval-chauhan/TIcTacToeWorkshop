9352f36b224e95018839fc81c01f74eb155c83db
Prajval Chauhan <prajval.chauhan3@gmail.com>
2020-10-08 14:22:58 +0530

    Logic for wining and drawing created but not complete

--- TicTacToeGame.cs
+++ TicTacToeGame.cs
@@ -8,8 +8,14 @@ namespace TicTacToeWorkshop
     {
         public static void Main(string[] args)
         {
+            //position vasriable for marking the move on board
+            int position;
+            int filledPositions = 0;
+            int checkResult;
+
             Console.WriteLine("Welcome to the Tic Tac Toe Program");
             char[] currentBoard = CreateBoard();
+            
             char humanInput = ZeroOrCross();
             char computerInput;
             if (humanInput == 'X')
@@ -19,25 +25,126 @@ namespace TicTacToeWorkshop
             else
                 computerInput = 'X';
             Console.WriteLine("Computer will play with : " + computerInput);
+            
             DispBoard(currentBoard);
-            Console.WriteLine("Choose H or T:");
+            
+            Console.WriteLine("\nChoose H or T:");
             char userToss = Convert.ToChar(Console.ReadLine());
             int tossResult = Toss();
             if(tossResult == 0)
                 Console.WriteLine("You Lost the Toss");
             else
                 Console.WriteLine("You won the Toss");
+
             
-            int position = User_Input();
-            if (currentBoard[position] == ' ')
-                currentBoard[position] = humanInput;
-            else
+            
+            switch(tossResult)
             {
-                Console.WriteLine("Position not empty");
-                position = User_Input();
+                case 0:
+                    for (; ; )
+                    {
+                        position = ComputerInput();
+                        if (currentBoard[position] == ' ')
+                            currentBoard[position] = computerInput;
+                        else
+                        {
+                            position = ComputerInput();
+                        }
+                        DispBoard(currentBoard);
+                        checkResult = resultCheck(currentBoard);
+                        if(checkResult == 0)
+                        {
+                            Console.WriteLine("Computer Won");
+                            break;
+                        }
+
+                        
+                        filledPositions = filledPositions + 1;
+                        if (filledPositions == 9)
+                        {
+                            Console.WriteLine("Match Drawn");
+                            break;
+                        }
+                        // User Moves
+                        position = User_Input();
+                        if (currentBoard[position] == ' ')
+                            currentBoard[position] = humanInput;
+                        else
+                        {
+                            Console.WriteLine("Position not empty");
+                            position = User_Input();
+                        }
+                        DispBoard(currentBoard);
+                        checkResult = resultCheck(currentBoard);
+                        if (checkResult == 0)
+                        {
+                            Console.WriteLine("User Won");
+                            break;
+                        }
+                        Console.Read();
+                        filledPositions = filledPositions + 1;
+
+                        if (filledPositions == 9)
+                        {
+                            Console.WriteLine("Match Drawn");
+                            break;
+                        }
+                    }
+                        break;
+                    
+
+                case 1:
+                    for (; ; )
+                    {
+                        position = User_Input();
+                        if (currentBoard[position] == ' ')
+                            currentBoard[position] = humanInput;
+                        else
+                        {
+                            Console.WriteLine("Position not empty");
+                            position = User_Input();
+                        }
+                        filledPositions = filledPositions + 1;
+                        DispBoard(currentBoard);
+                        checkResult = resultCheck(currentBoard);
+
+                        if (checkResult == 0)
+                        {
+                            Console.WriteLine("User Won");
+                            break;
+                        }
+                        if (filledPositions == 9)
+                        {
+                            Console.WriteLine("Match Drawn");
+                            break;
+                        }
+
+                        position = ComputerInput();
+                        if (currentBoard[position] == ' ')
+                            currentBoard[position] = computerInput;
+                        else
+                        {
+                            position = ComputerInput();
+                        }
+                        DispBoard(currentBoard);
+                        checkResult = resultCheck(currentBoard);
+                        if (checkResult == 0)
+                        {
+                            Console.WriteLine("Computer Won");
+                            break;
+                        }
+                        Console.ReadKey();
+                        if (filledPositions == 9)
+                        {
+                            Console.WriteLine("Match Drawn");
+                            break;
+                        }
+                    }
+                    break;
+
+
             }
-            Console.Clear();
-            DispBoard(currentBoard);
+            
 
         }
         /// <summary>
@@ -91,6 +198,29 @@ namespace TicTacToeWorkshop
             Random randomNum = new Random();
             int Toss = randomNum.Next(0, 1);
             return (Toss);
+        }
+
+        public static int ComputerInput()
+        {
+            Random randomNum = new Random();
+
+            Console.WriteLine("Computer's Turn: ");
+            int compMove = randomNum.Next(1, 10);
+            return compMove;
+        }
+
+        public static int resultCheck(char [] board)
+        {
+            int playResult = 1;
+            if ((board[1] == board[2] && board[2] == board[3]) || (board[4] == board[5] && board[5] == board[6])
+                || (board[7] == board[8] && board[8] == board[9]) || (board[1] == board[4] && board[4] == board[7])
+                || (board[2] == board[5] && board[5] == board[8]) || (board[3] == board[6] && board[6] == board[9])
+                || (board[1] == board[5] && board[5] == board[9]) || (board[3] == board[5] && board[5] == board[7])
+                )
+                playResult = 0;
+            
+            return playResult;
+
         }
     }
 }
