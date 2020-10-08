6ab10109e74aff021d5f4f6d53b2b27e6e361440
Prajval Chauhan <prajval.chauhan3@gmail.com>
2020-10-08 10:10:14 +0530

    board of size 10 created

--- TicTacToeGame.cs
+++ TicTacToeGame.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Runtime.ExceptionServices;
 
 namespace TicTacToeWorkshop
 {
@@ -7,6 +8,15 @@ namespace TicTacToeWorkshop
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the Tic Tac Toe Program");
+        }
+
+        public static void UC1()
+        {
+            char[] board = new char[10];
+            for (int i=0; i<10; i++)
+            {
+                board[i] = ' ';
+            }
         }
     }
 }
